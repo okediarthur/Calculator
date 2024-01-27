95373671cd2af38164286f8a4465fe4c315f3377
okediarthur <okediarthur@gmail.com>
2024-01-27 12:16:11 +0300

    Js Modification

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const keys = document.querySelectorAll('.key');
 const display_input = document.querySelector('.display .input');
-const display_output = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
 
 let input = "";
 
@@ -15,19 +15,17 @@ for(let key of keys){
         } else if(value == "backspace"){
             input = input.slice(0, -1);
             display_input.innerHTML = CleanInput(input);
-        } else if(value == "="){
+        } else if (value == "="){
             let result = eval(PrepareInput(input));
-
-            display_input.innerHTML = CleanOutput(result);
-        } else if (value == "brackets"){
+            display_output.innerHTML = CleanOutput(result);
+        } else if(value == "brackets"){
             if(
-                input.indexOf("(") != -1 &&
-                input.indexOf(")") == -1 ||
+                input.indexOf("(") == -1 ||
                 input.indexOf("(") != -1 &&
                 input.indexOf(")") != -1 &&
-                input.lastIndexOf("(") > input.lastIndexOf(")")
+                input.lastIndexOf("(") < input.lastIndexOf(")")
             ){
-                input += ")";
+                input += "(";
             } else if (
                 input.indexOf("(") != -1 &&
                 input.indexOf(")") == -1 ||
@@ -38,36 +36,35 @@ for(let key of keys){
                 input += ")";
             }
             display_input.innerHTML = CleanInput(input);
-        } else {
+        }else {
             if(ValidateInput(value)){
                 input += value;
                 display_input.innerHTML = CleanInput(input);
             }
         }
+        
     })
 }
 
 function CleanInput(input){
-    let input_array = input.split("")
+    let input_array = input.split("");
     let input_array_length = input_array.length;
 
-    for(let i = 0; i < input_array_length; i++){
+    for(let i = 0; i < input_array; i++){
         if(input_array[i] == "*"){
-            input_array[i] = `<span class="operator">x</span>`;
+            input_array[i] = `<span class= "operator">x</span> `;
         } else if (input_array[i] == "/"){
-            input_array[i] = `<span class="operator>÷</span>`;
-        }
-        else if (input_array[i] == "-"){
-            input_array[i] = `<span class="operator>-</span>`;
-        }
-        else if (input_array[i] == "+"){
-            input_array[i] = `<span class="operator>+</span>`;
-        } else if(input_array[i] == "("){
-            input_array[i] = `<span class="brackets>(</span>`;
-        } else if(input_array[i] == ")"){
-            input_array[i] = `<span class="brackets>)</span>`;
-        } else if(input_array[i] == "%"){
-            input_array[i] = `<span class="percent">%</span>`
+            input_array[i] = `<span class= "operator">÷</span> `;
+        } else if(input_array[i] == "+"){
+            input_array[i] == `<span class= "operator">+</span> `;
+        } else if(input_array[i] == "-"){
+            input_array[i] == `<spna class= "operator">-</span> `;
+        }else if(input_array[i] == "("){
+            input_array[i] == `<span class= "brackets">(</span>`;
+        }else if(input_array[i] == ")"){
+            input_array[i] == `<span class= "brackets">)</span>`;
+        }else if(input_array[i] == "%"){
+            input_array[i] == `<span class = "percent">%</span>`;
         }
     }
     return input_array.join("");
@@ -76,14 +73,14 @@ function CleanInput(input){
 function CleanOutput(output){
     let output_string = output.toString();
     let decimal = output_string.split(".")[1];
-    output_string = output.split(".")[0];
+    output_string = output_string.split(".")[0];
 
     let output_array = output_string.split("");
+
     if(output_array.length > 3){
         for(let i = output_array.length - 3; i > 0; i -= 3){
             output_array.splice(i, 0, ",");
         }
-        
     }
     if(decimal){
         output_array.push(".");
@@ -95,7 +92,7 @@ function CleanOutput(output){
 function ValidateInput(value){
     let last_input = input.slice(-1);
     let operators = ["+", "-", "*", "/"];
-    
+
     if(value == "." && last_input == "."){
         return false;
     }
@@ -106,15 +103,17 @@ function ValidateInput(value){
             return true;
         }
     }
+    return true;
 }
 
 function PrepareInput(input){
-    let input_array = input.slice("");
+    let input_array = input.split("");
 
     for(let i = 0; i < input_array.length; i++){
         if(input_array[i] == "%"){
-            input_array[i] = "/100";
+            input_array = "/100";
         }
     }
+
     return input_array.join("");
 }
